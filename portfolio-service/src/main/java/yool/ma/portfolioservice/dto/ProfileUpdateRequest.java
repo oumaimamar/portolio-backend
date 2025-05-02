eac98fd3f3d69c064061dc4be99b883884b8d7bb
oumaimamar <oumaimamarzouk02@gmail.com>
2025-05-02 17:03:01 +0100

    -- NUN

--- portfolio-service/src/main/java/yool/ma/portfolioservice/dto/ProfileUpdateRequest.java
+++ portfolio-service/src/main/java/yool/ma/portfolioservice/dto/ProfileUpdateRequest.java
@@ -1,17 +1,28 @@
 package yool.ma.portfolioservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 import java.util.List;
 
 @Data
 public class ProfileUpdateRequest {
     private String firstName;
     private String lastName;
+
+    @Email
     private String email;
 
+    @Pattern(regexp = "^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$", message = "Invalid phone number format")
     private String phoneNumber;
+
     private String diploma;
     private String profilePicture;
+
+    @Size(max = 500)
     private String bio;
+
     private List<String> socialLinks;
 }
